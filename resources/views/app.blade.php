9a50ed569c9d2a84a76ae3db254a55a2ff05bee1
gabri <impala67>
2019-08-08 22:28:08 -0300

    app todo em uma aplicação laravel

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,7 +8,7 @@
 
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
         <style>
             a:hover{
                 text-decoration: none!important;
@@ -17,6 +17,6 @@
     </head>
     <body>
     <div id=app></div>
-    <script src="{{asset('js/app.js')}}"></script>
+    <script src="{{ secure_asset('js/app.js')}}"></script>
     </body>
 </html>
