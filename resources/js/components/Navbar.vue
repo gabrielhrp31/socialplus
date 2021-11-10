95f42520135ec07c16f65f9826785eefee4e0754
Gabriel Rodrigues <0040892@academico.ifmg.edu.br>
2021-11-10 19:14:54 -0300

    ajustando tamanho da bottom bar

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,60 +1,5 @@
 <template>
     <div id="navbarRoot">
-<!--        <v-navigation-drawer-->
-<!--            app-->
-<!--            :expand-on-hover="true"-->
-<!--            v-model="drawer"-->
-<!--            width="200"-->
-<!--        >-->
-<!--            <v-list>-->
-<!--                <v-list-item class="px-0 d-flex justify-center">-->
-<!--                    <v-list-item-avatar>-->
-<!--                        <v-img :src="user.image"></v-img>-->
-<!--                    </v-list-item-avatar>-->
-<!--                    &lt;!&ndash;<span @click="drawer=!drawer"><v-icon small color="danger">mdi-close</v-icon></span>&ndash;&gt;-->
-<!--                </v-list-item>-->
-
-<!--                <v-list-item link to="/profile">-->
-<!--                    <v-list-item-content>-->
-<!--                        <v-list-item-title class="title">{{-->
-<!--                            user.name-->
-<!--                        }}</v-list-item-title>-->
-<!--                        <v-list-item-subtitle>{{-->
-<!--                            user.email-->
-<!--                        }}</v-list-item-subtitle>-->
-<!--                    </v-list-item-content>-->
-<!--                </v-list-item>-->
-<!--            </v-list>-->
-<!--            <v-divider></v-divider>-->
-<!--            <v-list nav dense>-->
-<!--                <v-list-item-group v-model="item" color="primary">-->
-<!--                    <v-list-item-->
-<!--                        v-for="(item, i) in items"-->
-<!--                        :key="i"-->
-<!--                        :to="item.link ? getLink(item) : '#'"-->
-<!--                    >-->
-<!--                        <v-list-item-icon>-->
-<!--                            <v-icon v-text="item.icon"></v-icon>-->
-<!--                        </v-list-item-icon>-->
-
-<!--                        <v-list-item-content>-->
-<!--                            <v-list-item-title-->
-<!--                                v-text="item.text"-->
-<!--                            ></v-list-item-title>-->
-<!--                        </v-list-item-content>-->
-<!--                    </v-list-item>-->
-<!--                    <v-list-item @click="logout">-->
-<!--                        <v-list-item-icon>-->
-<!--                            <v-icon>mdi-arrow-left</v-icon>-->
-<!--                        </v-list-item-icon>-->
-
-<!--                        <v-list-item-content>-->
-<!--                            <v-list-item-title>Sair</v-list-item-title>-->
-<!--                        </v-list-item-content>-->
-<!--                    </v-list-item>-->
-<!--                </v-list-item-group>-->
-<!--            </v-list>-->
-<!--        </v-navigation-drawer>-->
         <v-app-bar
             app
             inset
@@ -62,10 +7,6 @@
             :elevate-on-scroll="true"
             id="navbar"
         >
-<!--            <v-app-bar-nav-icon-->
-<!--                class="hidden-lg-and-up"-->
-<!--                @click.stop="drawer = !drawer"-->
-<!--            ></v-app-bar-nav-icon>-->
             <v-container class="fill-height py-0">
                 <v-row no-gutters align="center" class="fill-height">
                     <v-toolbar-title
@@ -119,11 +60,11 @@
 
                 >
                     <v-tab :key="i" :to="item.link ? getLink(item) : '#'">
-                        <v-icon size="36">{{ item.icon }}</v-icon>
+                        <v-icon size="30">{{ item.icon }}</v-icon>
                     </v-tab>
                 </template>
                 <v-tab to="/profile">
-                    <v-avatar size="36">
+                    <v-avatar size="30">
                         <v-img :src="user.image"></v-img>
                     </v-avatar>
                 </v-tab>
@@ -202,7 +143,7 @@ span {
     bottom: 0;
 }
 >>>.bottom-bar .v-tabs .v-tabs-bar{
-    height: 80px;
+    height: 60px;
 }
 
 >>> .theme-switch .v-messages{
